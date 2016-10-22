2a2ea091653148bf9884069d9b40d06c188b1799
Alex Scott <codetheorist@gmail.com>
2016-10-22 02:28:08 +0100

    Add home page banner & typed.js

---
+++ assets/javascripts/app-home.js
@@ -0,0 +1,15 @@
+
+  $(document).ready(function(){
+      $("#typed-inner").typed({
+        strings: ["<h1>CodeTheorist</h1>"],
+        typeSpeed: 50,
+        showCursor: false,
+        callback: function() {
+          $("#typed-inner-sub").typed({
+            strings: ["<h3>Code</h3>", "<h3>Build</h3>", "<h3>Deploy</h3>","<h3>Code - Build - Deploy</h3>"],
+            typeSpeed: 5,
+            startDelay: 300,
+          });
+        }
+      });
+  });
--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,1 +1,15 @@
-$(document).foundation()
+$(document).foundation();
+$(window).resize(function() {
+  setBannerSize();
+})
+$(window).ready(function() {
+  setBannerSize();
+})
+function setBannerSize() {
+  var wh = $(window).height();
+  var ww = $(window).width();
+  var tbh = $('#top-bar').height();
+  var total_height = wh - tbh;
+  console.log(ww + 'x' + wh + '(\' + total_height\')');
+  $('#home-banner').css({'width': ww, 'height': total_height});
+}
