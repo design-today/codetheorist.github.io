d2b0b565561b8d3e29201f506f8d484d00830e80
Alex Scott <codetheorist@gmail.com>
2016-10-22 02:51:00 +0100

    Add home page banner & typed.js

--- assets/javascripts/app-home.js
+++ assets/javascripts/app-home.js
@@ -1,5 +1,6 @@
 
   $(document).ready(function(){
+      // $('body').css({'overflow':'hidden'});
       $("#typed-inner").typed({
         strings: ["<h1>CodeTheorist</h1>"],
         typeSpeed: 50,
@@ -7,9 +8,14 @@
         showCursor: false,
         callback: function() {
           $("#typed-inner-sub").typed({
-            strings: ["<h3>Code</h3>", "<h3>Build</h3>", "<h3>Deploy</h3>","<h3>Code - Build - Deploy</h3>"],
+            strings: ["<h3>Code</h3>", "<h3>Build</h3>", "<h3>Deploy</h3>","<h3>Code - Build - Deploy^1000</h3>"],
             typeSpeed: 5,
             startDelay: 300,
+            // callback: function() {
+            //   $('#typed-inner, #typed-inner-sub, .typed-cursor').fadeOut(250, '', true);
+            //   $('#home-banner').slideUp(500, '', true);
+            //   $('body').css({'overflow':'auto'});
+            // }
           });
         }
       });
--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -9,7 +9,7 @@ function setBannerSize() {
   var wh = $(window).height();
   var ww = $(window).width();
   var tbh = $('#top-bar').height();
-  var total_height = wh - tbh;
-  console.log(ww + 'x' + wh + '(\' + total_height\')');
+  var total_height = wh - (tbh * 3);
+  // console.log(ww + 'x' + wh + '(\'' + total_height + '\')');
   $('#home-banner').css({'width': ww, 'height': total_height});
 }
