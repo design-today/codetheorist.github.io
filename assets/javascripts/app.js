bf6a595602f18ff78ad82b49d3c1347449196333
Alex Scott <codetheorist@gmail.com>
2016-10-27 18:35:01 +0100

    Optimize images

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,16 +1,17 @@
 // Init Foundation 6
-$('.slicky').slick({
-  dots: true,
-  arrows: true,
-  infinite: false,
-  speed: 300,
-  infinite: true,
-  slidesToShow: 1,
-  slidesToScroll: 1,
-  autoplay: false,
-  autoplaySpeed: 4000,
-  lazyLoad: 'ondemand',
-  fade: true,
-  cssEase: 'linear'
-});
+  $('.slicky').slick({
+    dots: true,
+    arrows: true,
+    infinite: false,
+    speed: 300,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 4000,
+    lazyLoad: 'ondemand',
+    fade: true,
+    cssEase: 'linear'
+  });
+
 $(document).foundation();
