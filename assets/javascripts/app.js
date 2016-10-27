69e98db96e9ca64e78a4296cadef2759649732ce
Alex Scott <codetheorist@gmail.com>
2016-10-27 17:31:14 +0100

    Finish portfolio page structure & add some portfolio items.

--- assets/javascripts/app.js
+++ assets/javascripts/app.js
@@ -1,2 +1,16 @@
 // Init Foundation 6
+$('.slicky').slick({
+  dots: true,
+  arrows: true,
+  infinite: false,
+  speed: 300,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: false,
+  autoplaySpeed: 4000,
+  lazyLoad: 'ondemand',
+  fade: true,
+  cssEase: 'linear'
+});
 $(document).foundation();
