31a556f61f4d89ff64f69aa14e8ff5759a4969fa
codetheorist <codetheorist@gmail.com>
2016-11-07 15:45:09 +0000

    Front page styling

--- assets/javascripts/contact.js
+++ assets/javascripts/contact.js
@@ -1,25 +1,16 @@
-// Make input group labels equal length
-function resetLength() {
-  $('.input-group-label').each(function() {
-    $(this).css({'width': '0'});
-  });
-}
-
 function equalLengthLabels() {
   var label_max_width = 0;
-  resetLength(function(){
-    $('.input-group-label').each(function() {
-      var label_width = $(this).width();
-      if(label_width >= label_max_width) {
-          label_max_width = label_width;
-      }
-    });
-    $('.input-group-label').each(function() {
-      $(this).css({'width': label_max_width + 20, 'padding': 0});
-    });    
+  $('.input-group-label').css({'width': '0'});
+
+  $('.input-group-label').each(function() {
+    var label_width = $(this).width();
+    if(label_width >= label_max_width) {
+        label_max_width = label_width;
+    }
   });
+
+  $('.input-group-label').each(function() {
+    $(this).css({'width': label_max_width + 20, 'padding': 0});
+  });    
 }
 equalLengthLabels();
-$(window).resize(function() {
-  equalLengthLabels();
-});
