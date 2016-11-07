c65e4df87f7d348164d3a7295b1d267a68952ca6
codetheorist <codetheorist@gmail.com>
2016-11-07 15:34:51 +0000

    Front page styling

--- assets/javascripts/contact.js
+++ assets/javascripts/contact.js
@@ -7,15 +7,16 @@ function resetLength() {
 
 function equalLengthLabels() {
   var label_max_width = 0;
-  resetLength();
-  $('.input-group-label').each(function() {
-    var label_width = $(this).width();
-    if(label_width >= label_max_width) {
-        label_max_width = label_width;
-    }
-  });
-  $('.input-group-label').each(function() {
-    $(this).css({'width': label_max_width + 20, 'padding': 0});
+  resetLength(function(){
+    $('.input-group-label').each(function() {
+      var label_width = $(this).width();
+      if(label_width >= label_max_width) {
+          label_max_width = label_width;
+      }
+    });
+    $('.input-group-label').each(function() {
+      $(this).css({'width': label_max_width + 20, 'padding': 0});
+    });    
   });
 }
 equalLengthLabels();
